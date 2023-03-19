289ee4c14359872e0f033892ec4786e08fe22bb1
zelk12 <lenrchikz@mail.ru>
2023-03-20 00:20:24 +0400

    Создана документация. (вероятно дрянь потом еще рефакторинг)

--- Lnaguage.cs
+++ Lnaguage.cs
@@ -14,11 +14,21 @@ namespace TTSWithoutNeron
 {
     internal partial class Lnaguage
     {
+        /// <summary>
+        /// Основной класс языков.
+        /// </summary>
         public partial class ForWork
         {
             private StandartClass.Name _name = new StandartClass.Name();
             private StandartClass.Dictionarys _dictionary = new StandartClass.Dictionarys();
 
+            /// <summary>
+            /// Метод первоначального запуска языка,
+            /// -Загружает файл языка
+            /// -Создает массив с делением этого файла по строкам
+            /// -Создает словари языка, которые используются при работе
+            /// </summary>
+            /// <param name="path">Путь к директории где храниться файл языка.</param>
             public void Load(string path = "")
             {
                 StreamReader _langFile = new StreamReader(path);
@@ -30,6 +40,12 @@ namespace TTSWithoutNeron
                 _dictionary.Create(_langTextLines);
             }
 
+            /// <summary>
+            /// Из единого текстового файла с переносами,
+            /// создает массив с делением на отдельные строк.
+            /// </summary>
+            /// <param name="input">Текстовый файл где есть переносы по строкам.</param>
+            /// <returns>Массив строк из файла.</returns>
             private static string[] textToTextLines(string input)
             {
                 string pattern = @"(\r\n)+";
@@ -52,6 +68,9 @@ namespace TTSWithoutNeron
 
             private class StandartClass
             {
+                /// <summary>
+                /// Класс обозначающий имя файла
+                /// </summary>
                 public class Name
                 {
                     private static string _value = "N/A";
@@ -66,10 +85,16 @@ namespace TTSWithoutNeron
                         _value = newValue;
                     }
 
+                    /// <summary>
+                    /// Ищет в массиве строк имя языка.
+                    /// </summary>
+                    /// <param name="_langTextLines">Массив строк языкового файла.</param>
                     public void Find(string[] _langTextLines)
                     {
                         foreach (string line in _langTextLines)
                         {
+                            bool nameFinded = false;
+
                             string[] _splitedLine = line.Split(',');
 
                             IEnumerator _lines = _splitedLine.GetEnumerator();
@@ -80,12 +105,22 @@ namespace TTSWithoutNeron
                                 if (_lineText == Commands.get[Commands.Names.Name] && _lines.MoveNext())
                                 {
                                     _value = _lines.Current.ToString().Trim();
+                                    nameFinded = true;
+                                    break;
                                 }
+                            }
+
+                            if (nameFinded)
+                            {
+                                break;
                             }
                         }
                     }
                 }
 
+                /// <summary>
+                /// Словари используемые для основной работы TTS.
+                /// </summary>
                 public class Dictionarys
                 {
                     private Dictionary<string, string> _charsAndSound = new Dictionary<string, string>();
@@ -94,6 +129,10 @@ namespace TTSWithoutNeron
                     private Dictionary<string, string> _variable = new Dictionary<string, string>();
                     private Dictionary<string, string> _wordAndTranscription = new Dictionary<string, string>();
 
+                    /// <summary>
+                    /// Создает и заполняет словари.
+                    /// </summary>
+                    /// <param name="_langTextLines">Массив строк языкового файла.</param>
                     public void Create(string[] _langTextLines)
                     {
                         int state = -1;
@@ -118,6 +157,9 @@ namespace TTSWithoutNeron
                             {
                                 switch (state)
                                 {
+                                    /// <summary>
+                                    /// Заполняет словарь сочетанием символов и создаваемыми ими звуками.
+                                    /// </summary>
                                     case 0:
                                         RegexOptions options = RegexOptions.IgnoreCase;
 
@@ -128,6 +170,9 @@ namespace TTSWithoutNeron
                                         _charsAndSound.Add(_tempNewCharData, _tempGlossary[1]);
                                         break;
 
+                                    /// <summary>
+                                    /// Заполняет словарь сочетанием звуков и создаваемыми ими в сумме звуками.
+                                    /// </summary>
                                     case 1:
                                         options = RegexOptions.IgnoreCase;
 
@@ -138,6 +183,9 @@ namespace TTSWithoutNeron
                                         _soundAndSound.Add(_tempNewSoundData, _tempGlossary[1]);
                                         break;
 
+                                    /// <summary>
+                                    /// Заполняет словарь существующими переменными и их значениями.
+                                    /// </summary>
                                     case 2:
                                         options = RegexOptions.IgnoreCase;
 
@@ -147,6 +195,9 @@ namespace TTSWithoutNeron
                                         _variable.Add(_tempName, _tempData);
                                         break;
 
+                                    /// <summary>
+                                    /// Заполняет словарь словами и их транскрипциями.
+                                    /// </summary>
                                     case 3:
                                         options = RegexOptions.IgnoreCase;
 
@@ -156,6 +207,9 @@ namespace TTSWithoutNeron
                                         _wordAndTranscription.Add(_tempGlossary[0], _tempGlossary[1]);
                                         break;
 
+                                    /// <summary>
+                                    /// Пишет что данный вид состояния не найден.
+                                    /// </summary>
                                     default:
                                         Debug.WriteLine("State " + state.ToString() + " not found.");
                                         break;
